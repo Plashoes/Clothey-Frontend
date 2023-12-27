2acc54a8d3f244b85e5997a58807a054d3775bcb
eslam_alhendawy <eslam_alhendawy@outlook.com>
2023-12-27 17:49:22 +0200

    Signup Fully Functional

--- src/Components/SideMenu.jsx
+++ src/Components/SideMenu.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import StateContext from "../StateContext";
 import { Link } from "react-router-dom";
 import Drawer from "@mui/material/SwipeableDrawer";
 
 function Cart() {
+  const appState = useContext(StateContext);
   const [openMenu, setOpen] = useState(false);
   const toggleMenu = (openStatus) => (event) => {
     if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
@@ -21,11 +23,24 @@ function Cart() {
             <i className="fa-solid fa-x text-3xl "></i>
           </button>
           <ul className="text-[#979a9b] ">
-            <Link to="/login" onClick={toggleMenu(false)}>
+            {/* <Link to="/login" onClick={toggleMenu(false)}>
               <li className="text-xl border-b-2 border-[#e6e6e6] px-8 py-4 hover:text-[#212529] duration-300">
                 <i className="fa-solid fa-user text"></i>
               </li>
-            </Link>
+            </Link> */}
+            {appState.loggedIn ? (
+              <Link to="/profile" onClick={toggleMenu(false)}>
+                <li className="text-xl border-b-2 border-[#e6e6e6] px-8 py-4 hover:text-[#212529] duration-300">
+                  <i className="fa-solid fa-user lg:text-lg xl:text-3xl cursor-pointer"></i>
+                </li>
+              </Link>
+            ) : (
+              <Link to="/login" onClick={toggleMenu(false)}>
+              <li className="text-xl border-b-2 border-[#e6e6e6] px-8 py-4 hover:text-[#212529] duration-300">
+                  <p>LOGIN</p>
+                </li>
+              </Link>
+            )}
             <Link to="/collection" onClick={toggleMenu(false)}>
               <li className="text-xl border-b-2 border-[#e6e6e6] px-8 py-4 hover:text-[#212529] duration-300">
                 <p>COLLECTION</p>
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,5 +1,5 @@
 import { toast } from "react-toastify";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import "react-toastify/dist/ReactToastify.css";
 import { useState, useContext } from "react";
@@ -8,6 +8,8 @@ import axios from "axios";
 
 function Signup() {
   const appDispatch = useContext(DispatchContext);
+  const navigate = useNavigate();
+  
   const URL = "https://clotheyapi-production.up.railway.app/users/signup";
   const regCharectars = /^[A-Za-z\s]*$/;
   const regNumbers = /^[0-9]+$/;
@@ -19,68 +21,71 @@ function Signup() {
   const [password, setPassword] = useState("");
   const [repeat, setRepeat] = useState("");
   const attemptSignup = () => {
-    if(first_name === ""){
+    if (first_name === "") {
       toast.error("Enter First Name");
       return;
     }
-    if(!regCharectars.test(first_name)){
+    if (!regCharectars.test(first_name)) {
       toast.error("Enter Correct First Name");
       return;
     }
-    if(last_name === ""){
+    if (last_name === "") {
       toast.error("Enter Last Name");
       return;
     }
-    if(!regCharectars.test(last_name)){
+    if (!regCharectars.test(last_name)) {
       toast.error("Enter Correct Last Name");
       return;
     }
-    if(email === ""){
+    if (email === "") {
       toast.error("Enter Email");
       return;
     }
-    if(!regEmail.test(email)){
+    if (!regEmail.test(email)) {
       toast.error("Enter Correct Email");
       return;
     }
-    if(phone_number === ""){
+    if (phone_number === "") {
       toast.error("Enter Phone Number");
       return;
     }
-    if(!regNumbers.test(phone_number) || phone_number.length != 11){
+    if (!regNumbers.test(phone_number) || phone_number.length != 11) {
       toast.error("Enter Correct Phone Number");
       return;
     }
-    if(password === ""){
+    if (password === "") {
       toast.error("Enter Password");
       return;
     }
-    if(password.length < 8){
+    if (password.length < 8) {
       toast.error("Password Must Be At Least 8 Charrecters Long");
       return;
     }
-    if(repeat === ""){
+    if (repeat === "") {
       toast.error("Repeat Password Please");
       return;
     }
-    if(password != repeat){
+    if (password != repeat) {
       toast.error("Password Does Not Match");
       return;
     }
     toast.info("Attempting Sign Up");
-    axios.post(URL, {first_name, last_name, email, password, phone_number}).then((res) => {
-      toast.success("Sign Up Successful!");
-      console.log(res);
-        appDispatch({type: "login"});
+    axios
+      .post(URL, { first_name, last_name, email, password, phone_number })
+      .then((res) => {
+        toast.success("Sign Up Successful!");
+        appDispatch({ type: "login" });
         localStorage.setItem("email", res.data.email);
         localStorage.setItem("firstName", res.data.first_name);
         localStorage.setItem("lastName", res.data.last_name);
         localStorage.setItem("phoneNumber", res.data.phone_number);
         localStorage.setItem("userToken", res.data.authorization_token);
-    }).catch((e) => {
-      console.log(e);
-      toast.error("Error Occurred Please Try Again");
-    })
+        navigate("/profile");
+      })
+      .catch((e) => {
+        console.log(e);
+        toast.error("Error Occurred Please Try Again");
+      });
     // toast.success("Login Successful");
     // toast.error("Login Failed");
   };
@@ -96,17 +101,17 @@ function Signup() {
             </div>
             <div className="flex flex-col space-y-2 mb-5 md:w-[300px]">
               <span className="sm:text-lg font-semibold text-[#212529]">Last Name</span>
-              <input className="bg-white border-b-2 border-[#e6e6e6] focus:placeholder-transparent placeholder:duration-300 focus:outline-none" placeholder="Enter Last Name" type="text" onChange={(e) => setLast(e.target.value)}  />
+              <input className="bg-white border-b-2 border-[#e6e6e6] focus:placeholder-transparent placeholder:duration-300 focus:outline-none" placeholder="Enter Last Name" type="text" onChange={(e) => setLast(e.target.value)} />
             </div>
           </div>
           <div className="flex flex-col sm:flex-row sm:space-x-6">
             <div className="flex flex-col space-y-2 mb-5 md:w-[300px]">
               <span className="sm:text-lg font-semibold text-[#212529]">Email</span>
-              <input className="bg-white border-b-2 border-[#e6e6e6] focus:placeholder-transparent placeholder:duration-300 focus:outline-none" placeholder="Enter Email" type="text" onChange={(e) => setEmail(e.target.value)}  />
+              <input className="bg-white border-b-2 border-[#e6e6e6] focus:placeholder-transparent placeholder:duration-300 focus:outline-none" placeholder="Enter Email" type="text" onChange={(e) => setEmail(e.target.value)} />
             </div>
             <div className="flex flex-col space-y-2 mb-5 md:w-[300px]">
               <span className="sm:text-lg font-semibold text-[#212529]">Phone Number</span>
-              <input className="bg-white border-b-2 border-[#e6e6e6] focus:placeholder-transparent placeholder:duration-300 focus:outline-none" placeholder="Enter Phone Number" type="text" onChange={(e) => setPhone(e.target.value)}  />
+              <input className="bg-white border-b-2 border-[#e6e6e6] focus:placeholder-transparent placeholder:duration-300 focus:outline-none" placeholder="Enter Phone Number" type="text" onChange={(e) => setPhone(e.target.value)} />
             </div>
           </div>
           <div className="flex flex-col sm:flex-row sm:space-x-6">
