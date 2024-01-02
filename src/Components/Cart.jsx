a8ccefd8efec71471240989d14cbb6a61441d1ad
eslam_alhendawy <eslam_alhendawy@outlook.com>
2024-01-02 06:51:44 +0200

    Handled empty cart case

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -3,15 +3,13 @@ import { Link } from "react-router-dom";
 import StateContext from "../StateContext";
 import axios from "axios";
 import Drawer from "@mui/material/SwipeableDrawer";
-import Skeleton from "@mui/material/Skeleton";
 
 function Cart() {
   const appState = useContext(StateContext);
   const [openCart, setOpen] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(null);
   const userToken = localStorage.getItem("userToken");
   const url = "https://clotheyapi-production.up.railway.app/carts/get-one";
-  const [fetching, setFetching] = useState(true);
 
   const toggleCart = (openStatus) => (event) => {
     if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
@@ -21,7 +19,6 @@ function Cart() {
   };
 
   useEffect(() => {
-    setFetching(true);
     const fetchCart = async () => {
       await axios
         .get(url, {
@@ -31,7 +28,6 @@ function Cart() {
         })
         .then((res) => {
           setCartItems(res.data.items);
-          setFetching(false);
         })
         .catch((e) => {
           console.log(e);
@@ -56,52 +52,64 @@ function Cart() {
             </button>
           </div>
           <div className="flex-grow pt-3 w-full">
-          {fetching ? (<>
-                  <div className="flex items-center space-x-2 px-6 py-3">
-                    <Skeleton variant="rectangle" animation="wave" width={75} height={75} />
-                    <div>
-                      <Skeleton variant="text" className="w-[100px] sm:w-[400px] md:w-[500px]" animation="wave" />
-                      <Skeleton variant="text" animation="wave" />
+            {cartItems === null ? (
+              <p className="text-center text-2xl font-semibold mt-20">Loading...</p>
+            ) : !Array.isArray(cartItems) ? (
+              <p className="text-center text-2xl font-semibold mt-20">Cart Is Empty</p>
+            ) : (
+              cartItems.map((item, index) => {
+                return (
+                  <div className="flex justify-between items-center mb-6" key={index}>
+                    <div className="flex items-center space-x-3">
+                      <div className="max-w-[90px]">
+                        <img src={item.product.main_image} alt="" />
+                      </div>
+                      <div>
+                        <p className="text-sm sm:text-lg font-semibold mb-3">{item.product.name}</p>
+                      </div>
+                    </div>
+                    <div className="">
+                      <div className="flex items-center space-x-2">
+                        <p className="text-sm sm:text-lg font-semibold">Price: </p>
+                        <p className="text-sm sm:text-lg font-semibold">${item.product.inventory.price}</p>
+                      </div>
+                      <div className="flex items-center space-x-2">
+                        <p className="text-sm sm:text-lg font-semibold">Quantity: </p>
+                        <p className="text-sm sm:text-lg font-semibold">{item.quantity}</p>
+                      </div>
                     </div>
                   </div>
-                  <div className="flex items-center space-x-2 px-6 py-3">
-                    <Skeleton variant="rectangle" animation="wave" width={75} height={75} />
-                    <div>
-                      <Skeleton variant="text" className="w-[100px] sm:w-[400px] md:w-[500px]" animation="wave" />
-                      <Skeleton variant="text" animation="wave" />
+                );
+              })
+            )}
+            {/* {cartItems && cartItems.length === 0  ? (
+                <p className="text-center text-2xl font-semibold mt-20">Cart Is Empty</p>
+            ) : (
+              cartItems.map((item, index) => {
+                return (
+                  <div className="flex justify-between items-center mb-6" key={index}>
+                    <div className="flex items-center space-x-3">
+                      <div className="max-w-[90px]">
+                        <img src={item.product.main_image} alt="" />
+                      </div>
+                      <div>
+                        <p className="text-sm sm:text-lg font-semibold mb-3">{item.product.name}</p>
+                      </div>
+                    </div>
+                    <div className="">
+                      <div className="flex items-center space-x-2">
+                        <p className="text-sm sm:text-lg font-semibold">Price: </p>
+                        <p className="text-sm sm:text-lg font-semibold">${item.product.inventory.price}</p>
+                      </div>
+                      <div className="flex items-center space-x-2">
+                        <p className="text-sm sm:text-lg font-semibold">Quantity: </p>
+                        <p className="text-sm sm:text-lg font-semibold">{item.quantity}</p>
+                      </div>
                     </div>
                   </div>
-                  <div className="flex items-center space-x-2 px-6 py-3">
-                    <Skeleton variant="rectangle" animation="wave" width={75} height={75} />
-                    <div>
-                      <Skeleton variant="text" className="w-[100px] sm:w-[400px] md:w-[500px]" animation="wave" />
-                      <Skeleton variant="text" animation="wave" />
-                    </div>
-                  </div>
-                </>) : (cartItems.map((item, index) => {
-              return (
-                <div className="flex justify-between items-center mb-6" key={index}>
-                  <div className="flex items-center space-x-3">
-                    <div className="max-w-[90px]">
-                      <img src={item.product.main_image} alt="" />
-                    </div>
-                    <div>
-                      <p className="text-sm sm:text-lg font-semibold mb-3">{item.product.name}</p>
-                    </div>
-                  </div>
-                  <div className="">
-                    <div className="flex items-center space-x-2">
-                      <p className="text-sm sm:text-lg font-semibold">Price: </p>
-                      <p className="text-sm sm:text-lg font-semibold">${item.product.inventory.price}</p>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                      <p className="text-sm sm:text-lg font-semibold">Quantity: </p>
-                      <p className="text-sm sm:text-lg font-semibold">{item.quantity}</p>
-                    </div>
-                  </div>
-                </div>
-              );
-            }))}
+                );
+              })
+            )} */}
           </div>
           <Link to="/cart" className="mb-6 bg-[#6e7051] hover:bg-[#212529] font-semibold text-center text-white duration-300 px-6 py-4" onClick={toggleCart(false)}>
             View cart
